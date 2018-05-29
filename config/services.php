a2236f542ae1156a3f05f9734539d520ca585db3
havok1305 <havok1305@gmail.com>
2018-05-29 20:15:52 -0300

    Updated readme. Altered the namespace

--- config/services.php
+++ config/services.php
@@ -30,7 +30,7 @@ return [
     ],
 
     'stripe' => [
-        'model' => App\User::class,
+        'model' => Stock\User::class,
         'key' => env('STRIPE_KEY'),
         'secret' => env('STRIPE_SECRET'),
     ],
