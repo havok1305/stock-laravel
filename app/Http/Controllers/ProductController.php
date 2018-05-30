d721952b31b4d071f7f5599af557b552bc548408
havok1305 <havok1305@gmail.com>
2018-05-29 21:26:01 -0300

    Testing database connection

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -2,12 +2,23 @@
 
 namespace Stock\Http\Controllers;
 
+use Illuminate\Support\Facades\DB;
 
 class ProductController extends Controller
 {
     public function lista()
     {
-        return "<h1>Listing some products</h1>";
+        $products = DB::select("SELECT * FROM products");
+        $html = "<h1>Listing some products</h1>";
+        $html .= "<ul>";
+        foreach($products as $product) {
+            $html .= "<li> Name: ".$product->name;
+            $html .= " Value: ".$product->value;
+            $html .= " Unit: ".$product->unit;
+            $html .= "</li>";
+        }
+        $html .= "</ul>";
+        return $html;
     }
 
 }
